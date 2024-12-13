99ebfb861bd1afb84d79098cc1cddc2af1c6d548
jdrupal-dev <13871894+jdrupal-dev@users.noreply.github.com>
2024-12-13 17:39:34 +0100

    Don't setup plugin when it's not enabled

--- lua/parcel/init.lua
+++ lua/parcel/init.lua
@@ -6,7 +6,10 @@ M.setup = function(options)
   require("parcel.config").__setup(options)
   M.enabled = require("parcel.config").options.default_enabled
 
-  require("parcel.outdated_packages").setup()
+  if M.enabled then
+    require("parcel.outdated_packages").setup()
+  end
+
   vim.api.nvim_create_user_command("ParcelToggle", function()
     M.enabled = not M.enabled
     require("parcel.outdated_packages").setup()
